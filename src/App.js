a2bc2be0dc4352190ece2ccfb7de4448d0a0eb5f
HugoTse <hugotse8@gmail.com>
2022-10-31 21:32:22 -0700

    web app up and running

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import '@aws-amplify/ui-react/styles.css';
+
+import {
+  withAuthenticator,
+  PasswordField,
+  ButtonGroup,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+  Grid,
+  TextField,
+  Alert,
+  Flex,
+  Badge,
+  Text,
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableBody,
+  ThemeProvider,
+  Theme,
+  SliderField,
+  TextAreaField,
+  SelectField,
+  SwitchField,
+  SearchField,
+  useTheme,
+  Divider,
+  ToggleButton,
+  TabItem,
+  Tabs
+} from "@aws-amplify/ui-react";
+
+import React, {
+  useState,
+  useEffect,
+} from "react";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>What's up</p>
+    </div>
+  );
+}
+
+export default App;
